e9de56e84c60103c38764fc09e3acc72bf7071f8
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-06-06 14:30:13 +0800

    chore: remove pronouns col from user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_06_061935) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_06_062859) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,7 +38,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_06_061935) do
 
   create_table "users", force: :cascade do |t|
     t.string "alias"
-    t.string "pronouns"
     t.text "bio"
     t.string "email"
     t.datetime "created_at", null: false
