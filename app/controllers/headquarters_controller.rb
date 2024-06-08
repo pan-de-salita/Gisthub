3d8087dafc92694d108fcce6fec58db64e615f03
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-06-08 13:39:21 +0800

    style: improve styling of overview page

--- app/controllers/headquarters_controller.rb
+++ app/controllers/headquarters_controller.rb
@@ -5,6 +5,7 @@ class HeadquartersController < ApplicationController
     @top_four_languages = @user.solutions.each_with_object(Hash.new(0)) do |solution, hash|
                             hash[solution.programming_language] += 1
                           end.sort_by { |_, value| -value }.first(4).to_h
+    # @top_four_languages = []
 
     months_count = Hash.new(0)
     %w[January February March April May June
