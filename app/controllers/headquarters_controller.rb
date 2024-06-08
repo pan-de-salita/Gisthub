b35543b09f7622895fe22e8406a20792c0d94e20
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-06-08 13:32:49 +0800

    feat: build overview page

--- app/controllers/headquarters_controller.rb
+++ app/controllers/headquarters_controller.rb
@@ -3,8 +3,17 @@ class HeadquartersController < ApplicationController
 
   def overview
     @top_four_languages = @user.solutions.each_with_object(Hash.new(0)) do |solution, hash|
-      hash[solution.programming_language] += 1
-    end.sort_by { |_, value| -value }.first(4).to_h
+                            hash[solution.programming_language] += 1
+                          end.sort_by { |_, value| -value }.first(4).to_h
+
+    months_count = Hash.new(0)
+    %w[January February March April May June
+       July August September October November December].each do |month|
+      months_count[month] = 0
+    end
+    @solutions = @user.solutions.each_with_object(months_count) do |solution, hash|
+      hash[solution.created_at.strftime('%B')] += 1
+    end
   end
 
   def solutions; end
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,11 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
+import "chartkick";
+import "Chart.bundle";
 
-const application = Application.start()
+const application = Application.start();
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+application.debug = false;
+window.Stimulus = application;
 
-export { application }
+export { application };
