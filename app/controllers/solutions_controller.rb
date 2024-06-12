fc327a0ff76ea4cc7dbbc8cfa1b17d4df2af93b9
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-06-12 11:44:30 +0800

    feat: add new solution page

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,13 +1,31 @@
 class SolutionsController < ApplicationController
-  before_action :set_user, :set_languages, :set_solutions
+  before_action :set_user, :set_languages, :set_solutions, :set_solution
 
   def solutions; end
 
-  def solutions_in_language
-    current_language_id = language_id
-    @solutions_in_language = Solution.where(user_id: @user.id, language_id: current_language_id,
-                                            programming_language: params[:programming_language])
+  def solutions_in_language; end
+
+  def new
+    @solution = Solution.new
   end
+
+  def create
+    @solution = Solution.new(solution_params)
+    @solution.tags = params[:solution][:tags].split(',') if params[:solution][:tags].present?
+
+    respond_to do |format|
+      if @solution.save
+        format.html { redirect_to solutions_path, notice: 'Solution was successfully created.' }
+        format.json { render :show, status: :created, location: @micropost }
+      else
+        puts "ERRORS: #{@solution.errors.full_messages}"
+        format.html { redirect_to solutions_path :new, status: :unprocessable_entity }
+        format.json { render json: @solution.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit; end
 
   private
 
@@ -21,5 +39,14 @@ class SolutionsController < ApplicationController
 
   def set_solutions
     @solutions = @user.solutions
+  end
+
+  def set_solution
+    @solution = Solution.new
+  end
+
+  def solution_params
+    params.require(:solution).permit(:title, :instructions, :attempt, :programming_language, :tags, :language_id,
+                                     :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   resources :users do
     resources :languages
     resources :solutions do
-      get 'in_language', action: :index
+      # get 'in_language', action: :index
     end
   end
 
   get '/overview/', to: 'headquarters#overview', as: 'overview'
   get 'solutions', to: 'solutions#index', as: 'solutions'
   get 'solutions/in_language/:programming_language', to: 'solutions#index', as: 'solutions_in_language'
+  get 'solutions/new', to: 'solutions#new', as: 'new_solution'
+  post 'solutions', to: 'solutions#create'
 end
